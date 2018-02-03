e3bccef281b687543b851840e1eed4f5a057427a
Maksim Milyutin <milyutinma@gmail.com>
2018-02-03 20:02:31 +0300

    Add example with scalable hash partition

---
+++ partition_methods/pg11/hash_parititioning.sql
@@ -0,0 +1,20 @@
+create table foo (i int) partition by hash (i);
+
+create table foo_1 partition of foo for values with (modulus 2, remainder 0);
+create table foo_2 partition of foo for values with (modulus 2, remainder 1);
+
+alter table foo detach partition foo_1;
+
+create table foo_1_1 partition of foo for values with (modulus 6, remainder 0);
+create table foo_1_2 partition of foo for values with (modulus 6, remainder 2);
+create table foo_1_3 partition of foo for values with (modulus 6, remainder 4);
+
+-- Move data from foo_1 to foo_1_*
+
+# explain (costs off) select * from foo where i = 34;
+        QUERY PLAN
+---------------------------
+ Append
+   ->  Seq Scan on foo_1_1
+         Filter: (i = 34)
+(3 rows)
