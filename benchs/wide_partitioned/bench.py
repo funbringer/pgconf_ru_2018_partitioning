feeed0f382968a4ddeb6a99d0d3712a114edc477
Maksim Milyutin <milyutinma@gmail.com>
2018-02-05 14:45:43 +0300

    Refactor and fix benchmark for iheritance

--- benchs/wide_partitioned/bench.py
+++ benchs/wide_partitioned/bench.py
@@ -1,48 +1,19 @@
 import testgres
-import random
 import tempfile
 import subprocess
 import os
 import re
 
+from inh import *
+
 dbname='postgres'
-
-class PartedTblState:
-
-    create_parted_tbl = 'create table foo(i int)'
-    create_part_i = 'create table foo_{0} (check (i={0})) inherits (foo)'
-    insert_part_i = 'insert into foo_{0} values ({0})'
-    select = 'select * from foo where i=%d'
-
-    def __init__(self, node):
-        self.created = False
-        self.partnum = 0
-
-    def set_node(self, node):
-        self.node = node
-
-    def create_tbl(self):
-        self.node.execute(dbname, self.create_parted_tbl)
-        self.created = True
-
-    def create_parts(self, num):
-        with self.node.connect() as con:
-            for i in range(self.partnum, num):
-                con.execute(self.create_part_i.format(i+1))
-                con.execute(self.insert_part_i.format(i+1))
-            con.commit()
-        self.partnum = num
-
-    def random_select(self):
-        return self.select % random.randrange(1, self.partnum+1)
-
 
 BENCH_DURATION = 100
 #  part_nums = [10] + range(100, 1001, 100)
 PART_NUMS = [100, 250, 500, 10**3, 2 * 10**3, 4 * 10**3, 8 * 10**3, 16 * 10**3]
 
 with testgres.get_new_node('master') as master:
-    pstate = PartedTblState(master)
+    pstate = InhPartedTblState()
 
     # start a new node
     master.init()
---
+++ benchs/wide_partitioned/inh.py
@@ -0,0 +1,35 @@
+import random
+
+dbname='postgres'
+
+class InhPartedTblState:
+
+    create_parted_tbl = 'create table foo(i int)'
+    create_part_i = 'create table foo_{0} (check (i={0})) inherits (foo)'
+    insert_part_i = 'insert into foo_{0} values ({0})'
+    select = 'select * from foo where i=%d'
+
+    def __init__(self):
+        self.created = False
+        self.partnum = 0
+
+    def set_node(self, node):
+        self.node = node
+        node.append_conf(
+                'postgresql.conf',
+                'max_locks_per_transaction=4096\n')
+
+    def create_tbl(self):
+        self.node.execute(dbname, self.create_parted_tbl)
+        self.created = True
+
+    def create_parts(self, num):
+        with self.node.connect() as con:
+            for i in range(self.partnum, num):
+                con.execute(self.create_part_i.format(i+1))
+                con.execute(self.insert_part_i.format(i+1))
+            con.commit()
+        self.partnum = num
+
+    def random_select(self):
+        return self.select % random.randrange(1, self.partnum+1)
